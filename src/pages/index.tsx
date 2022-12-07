27bf9efff7483367748756df33e50f260dd770b5
joaoricardoM <joao_pia_mds@hotmail.com>
2022-12-07 16:59:48 -0300

    update code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import styles from "./Cardapio.module.scss";
-import { ReactComponent as Logo } from "../assets/logo.svg"
+import { ReactComponent as Logo } from "../assets/logo.svg";
+import Buscardor from "./Buscador";
+import { useState } from "react";
 
 export default function Cardapio() {
-    return (
-        <main>
-            <nav className={styles.menu}>
-                <Logo />
-            </nav>
-        </main>
-    );
+  const [busca, setBusca] = useState("");
+
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do codigo e da massa</div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardapio</h3>
+        <Buscardor busca={busca} setBusca={setBusca} />
+      </section>
+    </main>
+  );
 }

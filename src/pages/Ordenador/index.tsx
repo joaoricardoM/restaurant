53b4cfc2707799d838a02ecfe60128fb31485b5c
joaoricardoM <joao_pia_mds@hotmail.com>
2022-12-14 16:56:28 -0300

    create Ordenar mode

---
+++ src/pages/Ordenador/index.tsx
@@ -0,0 +1,51 @@
+import styles from "./Ordenador.module.scss";
+import opcoes from "./opcoes.json";
+import React, { useState } from "react";
+import classNames from "classnames";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+
+interface Props {
+  ordenador: string;
+  setOrdenador: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Ordenador({ ordenador, setOrdenador }: Props) {
+  const [open, setOpen] = useState(false);
+  const nomeOrdenador =
+    ordenador && opcoes.find((option) => option.value === ordenador)?.nome;
+
+  return (
+    <button
+      className={classNames({
+        [styles.ordenador]: true,
+        [styles["ordenador--ativo"]]: ordenador !== "",
+      })}
+      onClick={() => setOpen(!open)}
+      onBlur={() => setOpen(false)}
+    >
+      <span> {nomeOrdenador || "Ordenar por"} </span>
+      {open ? (
+        <MdKeyboardArrowUp size={20} />
+      ) : (
+        <MdKeyboardArrowDown size={20} />
+      )}
+
+      <div
+        className={classNames({
+          [styles.ordenador__options]: true,
+          [styles["ordenador__options--ativo"]]: open,
+        })}
+      >
+        {opcoes.map((option) => (
+          <div
+            className={styles.ordenador__option}
+            key={option.value}
+            onClick={() => setOrdenador(option.value)}
+          >
+            {option.nome}
+          </div>
+        ))}
+      </div>
+    </button>
+  );
+}

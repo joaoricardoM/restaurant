3cdee91be720699633b851ffff7ae7066e9f9dc4
joaoricardoM <joao_pia_mds@hotmail.com>
2023-08-24 17:07:15 -0300

    .

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import Cardapio from '../itens/itens.json'
+
+export default function Home() {
+  return (
+    <section>
+      <h3>Recomendações da cozinha</h3>
+      <div></div>
+    </section>
+  )
+}
